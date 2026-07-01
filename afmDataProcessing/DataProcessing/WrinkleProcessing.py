1cf1839cf29f70a1920895f04b2baf63ceb9e50f
felipesanchez05 <fepesanchez05@proton.me>
2026-07-01 15:41:39 -0500

    Completed autmatic measurement of ASCII matrix .txt file

--- afmDataProcessing/DataProcessing/WrinkleProcessing.py
+++ afmDataProcessing/DataProcessing/WrinkleProcessing.py
@@ -9,8 +9,8 @@ import ruptures as rpt
 import matplotlib.pyplot as plt
 import scipy.optimize as sp
 #import Gwyddion data
-import pandas as pd
 from math import pi
+
 def piecewise_wrinkle(x,first_breakpoint,second_breakpoint, A, m_left, b_left, m_right, b_right):
     y = np.zeros_like(x)
     left_mask = x < first_breakpoint
@@ -24,79 +24,79 @@ def piecewise_wrinkle(x,first_breakpoint,second_breakpoint, A, m_left, b_left, m
     y[wrinkle_mask] = base + (A/2) * (1 + np.cos(2 * pi * (x[wrinkle_mask] - midpoint) / lam))
 
     return y
+E = 5.7*10**9
+strain = 0.03
+def adhesion(length, thickness, wvlength, amplitude):
+    aEnergy_1stTerm = ( (pi**4) * (amplitude**4) * (E)* thickness) / (16 * (wvlength) * length)
+    aEnergy_2ndTerm = ( (strain) * (pi**2) * (amplitude**2) * E * thickness ) / (4 * (wvlength**2))
+    aEnergy_3rdTerm = ( (pi**4) * (amplitude**2) * E * (thickness**3)) / (4 * wvlength**4)
+
+    adhesion_energy = aEnergy_1stTerm - aEnergy_2ndTerm + aEnergy_3rdTerm
+    return adhesion_energy,aEnergy_1stTerm, aEnergy_2ndTerm,aEnergy_3rdTerm
 
 input_file = input("Data file name? : ")
-pixel_horizontal_dimension = input("Size of pixel?: ")
+thickness =int(input("Thickness of the flake (in nm)?: "))
+thickness *= 10**(-9)
+length = 10*10**(-6) #standard length
+pixel_width = float(input("Width of pixel(in microns)?:"))
+pixel_width *= 10**(-6)
 
-df = pd.read_csv(input_file, 
-                 delimiter=";", 
-                 skiprows=3,
-                 header=None,
-                 names=["x1","y1","x2","y2","drop"],
-                 usecols=[0,1,2,3])  # ignore the trailing empty column
+#get x coordinate data
+data = np.loadtxt(input_file)
 
-x1 = df["x1"].values
-y1 = df["y1"].values 
-mask2 = df["x2"].notna()
-x2 = df["x2"][mask2].values
-y2 = df["y2"][mask2].values
+nx = data.shape[1]
+x_values = np.arange(nx) * pixel_width
 
+results = []
+for row in data:
+    try:    
+        dydx = np.gradient(row)
+        algo = rpt.Dynp(model='rbf').fit(dydx)
+        breakpoints = algo.predict(n_bkps=2)
+        #fit data
+        bp1_id = breakpoints[0]
+        bp2_id = breakpoints[1] - 1
+        bp1 = x_values[bp1_id]
+        bp2 = x_values[bp2_id]
 
-plt.plot(x1,y1)
-plt.plot(x2,y2)
-plt.savefig('output.png')
-plt.close()
-#find breakpoints
-dydx1 = np.gradient(y1,x1)
-dydx2 = np.gradient(y2,x2)
+        A_guess = np.max(row) - np.median(row)
+        baseline = np.median(row)
+        guess = [bp1, bp2, A_guess, 0, baseline, 0, baseline]
+        lower = [x_values[0], bp1,  0,      -np.inf, 0,       -np.inf, 0      ]
+        upper = [bp2,x_values[-1],  np.inf,  np.inf, np.inf,   np.inf, np.inf ]
+        popt, pcov = sp.curve_fit(piecewise_wrinkle, x_values , row, p0=guess, bounds=(lower,upper))
 
-algo = rpt.Dynp(model='rbf').fit(dydx1.reshape(-1,1))
-algo2 = rpt.Dynp(model='rbf').fit(dydx2.reshape(-1,1))
-breakpoints1 = algo.predict(n_bkps=2)
-breakpoints2 = algo2.predict(n_bkps=2)
-rpt.display(y1,breakpoints1)
-plt.savefig('output2.png')
-rpt.display(y2,breakpoints2)
-plt.savefig("output3.png")
-plt.close()   
+        #extract measurements
+        lam = popt[1] - popt[0]
+        amp = popt[2]
 
+        #calculate energy
+        adhesionE = adhesion(length, thickness, lam, amp) #returns [adhesion energy, first term, second term, third term]
 
-#fit data
-bp1_id = breakpoints1[0]
-bp2_id = breakpoints1[1]
-bp1 = x1[bp1_id]
-bp2 = x1[bp2_id-1]
-print(f"Wrinkle from {bp1*1e6:.2f} to {bp2*1e6:.2f} μm")
-A_guess = y1.max() - np.median(y1)
-baseline = np.median(y1)
-guess = [bp1, bp2, A_guess, 0, baseline, 0, baseline]
-lower = [bp1 * 0.8, bp2 * 0.8,  0,      -np.inf, 0,       -np.inf, 0      ]
-upper = [bp1 * 1.2, bp2 * 1.2,  np.inf,  np.inf, np.inf,   np.inf, np.inf ]
-popt, pcov = sp.curve_fit(piecewise_wrinkle, x1, y1, p0=guess, bounds=(lower,upper))
-yfit=piecewise_wrinkle(x1,*popt)
-plt.figure()
-plt.plot(x1,y1, 'bo', label ='data')
-plt.plot(x1,yfit, 'r-',label='fit')
-plt.savefig('output4.png')
-print(popt)
+        #append results
+        results.append({'A':amp, "wavelength":lam, "Adhesion energy":adhesionE[0] ,"length terms":adhesionE[1]})
+    except RuntimeError:
+        print("Failure to converge")
+        
 
-bp1_id_2 = breakpoints2[0]
-bp2_id_2 = breakpoints2[1]
-bp1_2 = x2[bp1_id_2]
-bp2_2 = x2[bp2_id_2-1]
-A_guess_2 = y2.max() - np.median(y2)
-baseline_2 = np.median(y2)
-guess_2 = [bp1_2, bp2_2, A_guess_2, 0, baseline_2, 0, baseline_2]
-lower_2 = [bp1_2 * 0.8, bp2_2 * 0.8,  0,      -np.inf, 0,       -np.inf, 0      ]
-upper_2 = [bp1_2 * 1.2, bp2_2 * 1.2,  np.inf,  np.inf, np.inf,   np.inf, np.inf ]
-popt2, pcov2 = sp.curve_fit(piecewise_wrinkle, x2, y2, p0=guess_2, bounds=(lower_2,upper_2))
-yfit2=piecewise_wrinkle(x2,*popt2)
-plt.figure()
-plt.plot(x2,y2, 'bo', label ='data')
-plt.plot(x2,yfit2, 'r-',label='fit')
-plt.savefig('output5.png')
-#extract measurements
-
-#calculate data
 
 #stats
+amplitudes = [r['A'] for r in results]
+wavelengths = [r['wavelength'] for r in results]
+AdhesionEnergies = [r['Adhesion energy'] for r in results]
+length_terms = [r['length terms'] for r in results]
+
+mean_amplitude = np.mean(amplitudes)
+mean_wavelength = np.mean(wavelengths)
+mean_adhesion_energy = np.mean(AdhesionEnergies)
+adhesion_energy_from_mean =  adhesion(length, thickness, mean_wavelength,mean_amplitude)
+greatest_first_term = np.max(length_terms)
+
+with open(f'{input_file}_results.txt', 'w') as f:
+    f.write(f"For {input_file}:\n")
+    f.write(f"Mean amplitude: {mean_amplitude:.2e} +/- {np.std(amplitudes):.2e} \n")
+    f.write(f"Mean wavelength: {mean_wavelength:.2e} +/- {np.std(wavelengths):.2e}\n")
+    f.write(f"Mean adhesion energy: {mean_adhesion_energy:.2e} +/- {np.std(AdhesionEnergies):.2e}\n")
+    f.write(f"Adhesion energy from means: {adhesion_energy_from_mean[0]:.2e}\n")
+    f.write(f"Greatest length term: {greatest_first_term:.2e}\n")
+
